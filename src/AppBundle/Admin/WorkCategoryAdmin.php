a76d920229f16bec642c56ac0fcfb1f0efb9c760
Anton Serra <aserratorta@gmail.com>
2016-02-27 00:19:11 +0100

    create WorkCategoryAdmin

---
+++ src/AppBundle/Admin/WorkCategoryAdmin.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use AppBundle\Admin\AbstractBaseAdmin;
+use Doctrine\ORM\QueryBuilder;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+/**
+ * Class WorkCategoryAdmin
+ *
+ * @category Admin
+ * @package  AppBundle\Admin
+ * @author   Anton Serra <aserratorta@gmail.com>
+ */
+class WorkCategoryAdmin extends AbstractBaseAdmin
+{
+    protected $classnameLabel = 'Category';
+    protected $baseRoutePattern = 'works/category';
+    protected $datagridValues = array(
+        '_sort_by'    => 'title',
+        '_sort_order' => 'desc',
+    );
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('backend.admin.category', $this->getFormMdSuccessBoxArray(6))
+            ->add(
+                'title',
+                null,
+                array(
+                    'label' => 'backend.admin.title',
+                )
+            )
+            ->end()
+            ->with('backend.admin.controls', $this->getFormMdSuccessBoxArray(6))
+            ->add(
+                'enabled',
+                'checkbox',
+                array(
+                    'label'    => 'backend.admin.enabled',
+                    'required' => false,
+                )
+            )
+            ->end();
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add(
+                'title',
+                null,
+                array(
+                    'label'    => 'backend.admin.title',
+                )
+            )
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label'    => 'backend.admin.enabled',
+                    'editable' => true,
+                )
+            );
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        unset($this->listModes['mosaic']);
+        $listMapper
+            ->add(
+                'title',
+                null,
+                array(
+                    'label'    => 'backend.admin.title',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label'    => 'backend.admin.enabled',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'actions' => array(
+                        'show'   => array(),
+                        'edit'   => array(),
+                        'delete' => array(),
+                    ),
+                )
+            );
+    }
+}
+
