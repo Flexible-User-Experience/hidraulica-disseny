68564c216afcb6d435e6fe0718490751803cf92b
David Romaní <david@flux.cat>
2016-03-07 17:07:53 +0100

    add cart repository

---
+++ src/AppBundle/Repository/Cart/CartItemRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ECVulco\AppBundle\Repository\Cart;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class CartItemRepository
+ *
+ * @category Repository
+ * @package  ECVulco\AppBundle\Repository\Cart
+ * @author   David Romaní <david@flux.cat>
+ */
+class CartItemRepository extends EntityRepository
+{
+}
---
+++ src/AppBundle/Repository/Cart/CartRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ECVulco\AppBundle\Repository\Cart;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class CartRepository
+ *
+ * @category Repository
+ * @package  ECVulco\AppBundle\Repository\Cart
+ * @author   David Romaní <david@flux.cat>
+ */
+class CartRepository extends EntityRepository
+{
+}
