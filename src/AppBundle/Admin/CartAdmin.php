c8f652a08d3ee16048ca5e68ade6e29d291cba67
Anton Serra <aserratorta@gmail.com>
2016-03-08 22:46:20 +0100

    create and set cartadmin

---
+++ src/AppBundle/Admin/CartAdmin.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+/**
+ * Class CartAdmin
+ *
+ * @category Admin
+ * @package  AppBundle\Admin
+ * @author   Anton Serra <aserratorta@gmail.com>
+ */
+class CartAdmin extends AbstractBaseAdmin
+{
+    protected $classnameLabel = 'Cart';
+    protected $baseRoutePattern = '/cart';
+    protected $datagridValues = array(
+        '_sort_by'    => 'createdAt',
+        '_sort_order' => 'desc',
+    );
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('backend.admin.general', $this->getFormMdSuccessBoxArray(9))
+            ->add(
+                'items',
+                null,
+                array(
+                    'label'    => 'backend.admin.items',
+                )
+            )
+            ->end()
+            ->with('backend.admin.controls', $this->getFormMdSuccessBoxArray(3))
+            ->add(
+                'createdAt',
+                'sonata_type_date_picker',
+                array(
+                    'label'  => 'backend.admin.created_date',
+                    'format' => 'd/M/y',
+                )
+            )
+            ->add(
+                'enabled',
+                'checkbox',
+                array(
+                    'label'    => 'backend.admin.enabled',
+                    'required' => false,
+                )
+            )
+            ->end();
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add(
+                'createdAt',
+                'doctrine_orm_date',
+                array(
+                    'label'      => 'backend.admin.created_date',
+                    'field_type' => 'sonata_type_date_picker',
+                    'format'     => 'd-m-Y',
+                )
+            )
+            ->add(
+                'items',
+                null,
+                array(
+                    'label'    => 'backend.admin.items',
+                )
+            )
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label'    => 'backend.admin.enabled',
+                    'editable' => true,
+                )
+            );
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        unset($this->listModes['mosaic']);
+        $listMapper
+            ->add(
+                'createdAt',
+                'date',
+                array(
+                    'label'    => 'backend.admin.created_date',
+                    'format'   => 'd/m/Y',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'createdAt',
+                'sonata_type_date_picker',
+                array(
+                    'label'  => 'backend.admin.created_date',
+                    'format' => 'd/M/y',
+                )
+            )
+            ->add(
+                'enabled',
+                'checkbox',
+                array(
+                    'label'    => 'backend.admin.enabled',
+                    'required' => false,
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'label' => 'backend.admin.actions',
+                    'actions' => array(
+                        'show'   => array(),
+                    ),
+                )
+            );
+    }
+}
+
