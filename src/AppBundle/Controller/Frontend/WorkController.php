aff4086a29a445720fbbed991ea993c3eb679ed5
Anton Serra <aserratorta@gmail.com>
2016-03-13 00:20:27 +0100

    create WorkController

---
+++ src/AppBundle/Controller/Frontend/WorkController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller\Frontend;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class WorkController extends Controller
+{
+    /**
+     * @Route("/work", name="work")
+     */
+    public function workListAction()
+    {
+        return $this->render('Frontend/Work/work.html.twig');
+    }
+
+    /**
+     * @Route("/work/{slug}", name="work_detail")
+     * @param $slug
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function workDetailAction()
+    {
+        return $this->render('Frontend/Work/work_detail.html.twig');
+    }
+}
