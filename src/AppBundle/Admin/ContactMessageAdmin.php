c0af68010165c4b563bd7bc79dea232c83ac7824
David Romaní <david@flux.cat>
2017-08-28 20:36:32 +0200

    fix sonata translations problem 2

--- src/AppBundle/Admin/ContactMessageAdmin.php
+++ src/AppBundle/Admin/ContactMessageAdmin.php
@@ -225,6 +225,7 @@ class ContactMessageAdmin extends AbstractBaseAdmin
                 '_action',
                 'actions',
                 array(
+                    'label' => 'backend.admin.actions',
                     'actions' => array(
                         'show'   => array(
                             'template' => '::Admin/Buttons/list__action_show_button.html.twig'
